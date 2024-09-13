0a364df22953f62b90bc01973c8da700bc18df0e
S-Amine <saidi.amine.p@gmail.com>
2024-09-13 16:02:16 +0200

    change

--- settings/settings.go
+++ settings/settings.go
@@ -2,7 +2,7 @@ package settings
 
 const (
 	MspID        = "Org1MSP"
-	CryptoPath   = "/root/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com"
+	CryptoPath   = "/home/nhms/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com"
 	CertPath     = CryptoPath + "/users/User1@org1.example.com/msp/signcerts"
 	KeyPath      = CryptoPath + "/users/User1@org1.example.com/msp/keystore"
 	TlsCertPath  = CryptoPath + "/peers/peer0.org1.example.com/tls/ca.crt"
